60735a581213f0060f68d955b042e2fde9da04dc
Pedrael <mihail_krivic@mail.ru>
2021-02-20 22:01:13 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -3,4 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAul_TVejF5FEBFqri9u3kTttLtgI2242w",
+    authDomain: "vue-firebase-app-c37f3.firebaseapp.com",
+    projectId: "vue-firebase-app-c37f3",
+    storageBucket: "vue-firebase-app-c37f3.appspot.com",
+    messagingSenderId: "887963953006",
+    appId: "1:887963953006:web:d3b6c03f19bdf3e7d7c37f"
+  }
+
+  firebase.initializeApp(firebaseConfig)
+
 createApp(App).use(store).use(router).mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import firebase from 'firebase/app'
 
 export default createStore({
   state: {
@@ -6,6 +7,19 @@ export default createStore({
   mutations: {
   },
   actions: {
+    login({dispatch, commit}, {email, password}) {
+      return new Promise((resolve, reject) => {
+        console.log('submitting store...')
+        firebase.auth().signInWithEmailAndPassword(email, password).then((r) => {
+          resolve(r)
+        }).catch((e) => {
+          reject(e)
+        })
+      })
+    }, 
+    async logout({dispatch, commit}) {
+      await firebase.auth().signOut()
+    }
   },
   modules: {
   }
